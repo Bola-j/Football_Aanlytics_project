9c449b8e7c353014e4751dd13ad60607b3724d28
Bola Ghaly <bolagerges221@gmail.com>
2025-12-12 19:16:26 +0200

    Project Setup

---
+++ scraping/scrape_matches_possession.py
@@ -0,0 +1,24 @@
+# Script to scrape matches and possession data?
+
+# Does possession correlate with match outcomes?
+
+# https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures
+
+"""
+COLLECTED DATA:
+    Player Name
+
+    Team
+
+    Position
+
+    Age
+
+    Goals
+
+    Assists
+
+    Minutes played
+
+    Matches played
+"""
---
+++ scraping/scrape_salaries.py
@@ -0,0 +1,18 @@
+# Script to scrape player salaries
+# Are salaries aligned with goals, assists, minutes, etc.?
+
+# https://www.transfermarkt.com/arsenal-fc/kader/verein/11/saison_id/2024/plus/1
+
+
+"""
+Collected DATA:
+    Player
+
+    Market value
+
+    Age
+
+    Contract value
+
+    Estimated salary (if listed)
+"""
